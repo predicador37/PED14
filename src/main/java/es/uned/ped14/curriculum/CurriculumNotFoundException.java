33f5c269ecade7466a7096b0cc5e7b608b98eca8
Miguel Expósito <exposito_m@cantabria.es>
2015-04-19 00:47:01 +0200

    Añadida excepción Curriculum no encontrado

---
+++ src/main/java/es/uned/ped14/curriculum/CurriculumNotFoundException.java
@@ -0,0 +1,11 @@
+package es.uned.ped14.curriculum;
+
+public class CurriculumNotFoundException extends Exception {
+
+	public CurriculumNotFoundException(String string) {
+		// TODO Auto-generated constructor stub
+		System.out.println(string);
+	}
+
+	
+}
