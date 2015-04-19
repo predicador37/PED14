774dc9f35353cfb9147c3b119e642c9ac93b88e8
Miguel Expósito <exposito_m@cantabria.es>
2015-04-19 10:53:22 +0200

    Creado primer test para método de búsqueda de currículos usando varios parámetros simples

--- src/test/java/es/uned/ped14/curriculum/CurriculumServiceTest.java
+++ src/test/java/es/uned/ped14/curriculum/CurriculumServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.junit.Rule;
@@ -16,6 +17,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CurriculumServiceTest {
@@ -39,7 +42,7 @@ public class CurriculumServiceTest {
 
 
 	@Test
-	public void shouldReturnCurriculum() throws CurriculumNotFoundException {
+	public void shouldReturnCurriculumById() throws CurriculumNotFoundException {
 		// arrange
 		Curriculum demoCurriculum = new Curriculum("Miguel", "Expósito Martín", "España", "Santander", "htp://localhost/imagen.png", "http://localhost/archivo.pdf");
 		when(curriculumRepositoryMock.findOne(1L)).thenReturn(demoCurriculum);
@@ -50,6 +53,27 @@ public class CurriculumServiceTest {
 		// assert
 		assertThat(demoCurriculum.getNombre()).isEqualTo(curriculum.getNombre());
 		assertThat(demoCurriculum.getCiudad()).isEqualTo(curriculum.getCiudad());
+	}
+	
+	@Test
+	public void shouldReturnCurriculumByPaisAndCiudad() throws CurriculumNotFoundException {
+		// arrange
+		Curriculum demoCurriculum1 = new Curriculum("Miguel", "Expósito", "España", "Santander", "htp://localhost/imagen.png", "http://localhost/archivo.pdf");
+		Curriculum demoCurriculum2 = new Curriculum("Héctor", "Garnacho", "España", "Valladolid", "htp://localhost/imagen.png", "http://localhost/archivo.pdf");
+		Curriculum demoCurriculum3 = new Curriculum("Marcos", "Azorí", "España", "Madrid", "htp://localhost/imagen.png", "http://localhost/archivo.pdf");
+		List<Curriculum> demoCurriculos = new ArrayList<Curriculum>();
+		demoCurriculos.add(demoCurriculum1);
+		
+		when(curriculumRepositoryMock.findByPaisAndCiudad("España", "Santander")).thenReturn(demoCurriculos);
+
+		// act
+		List<Curriculum> curriculos = curriculumService.findByPaisAndCiudad("España", "Santander");
+     
+		// assert
+		for (Curriculum c : curriculos) {
+			assertThat(demoCurriculum1.getNombre()).isEqualTo(c.getNombre());
+			assertThat(demoCurriculum1.getCiudad()).isEqualTo(c.getCiudad());
+		}
 	}
 
 	
