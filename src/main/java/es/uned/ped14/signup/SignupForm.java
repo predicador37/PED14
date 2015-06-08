9c769954cc9ff697c278f0219e559048c2a90fc2
Marcos Azorí <mazori@users.noreply.github.com>
2015-06-08 13:37:42 +0200

    Creado formulario dar de alta a un usuario. Hay que crear dos columnas nuevas en Account para que almacene el login del usuario y su provincia.

--- src/main/java/es/uned/ped14/signup/SignupForm.java
+++ src/main/java/es/uned/ped14/signup/SignupForm.java
@@ -8,6 +8,8 @@ public class SignupForm {
 
 	private static final String NOT_BLANK_MESSAGE = "{notBlank.message}";
 	private static final String EMAIL_MESSAGE = "{email.message}";
+	private static final String USUARIO_MESSAGE = "{usuario.message}";
+	private static final String PROVINCIA_MESSAGE = "{provincia.message}";
 
     @NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
 	@Email(message = SignupForm.EMAIL_MESSAGE)
@@ -15,6 +17,12 @@ public class SignupForm {
 
     @NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
 	private String password;
+    
+    @NotBlank(message = SignupForm.USUARIO_MESSAGE)
+	private String usuario;
+    
+    @NotBlank(message = SignupForm.PROVINCIA_MESSAGE)
+	private String provincia;
 
     public String getEmail() {
 		return email;
@@ -32,7 +40,24 @@ public class SignupForm {
 		this.password = password;
 	}
 
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
 	public Account createAccount() {
         return new Account(getEmail(), getPassword(), "ROLE_USER");
 	}
+
 }
